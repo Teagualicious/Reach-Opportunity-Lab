26685ae6307520e7fb15050844352dd3bc57e333
Jeremy burris <jeremyburris81@gmail.com>
2026-07-15 20:14:23 -0400

    Add automatic GitHub Pages preview (#7)

--- src/data/StaticZctaGeometrySource.ts
+++ src/data/StaticZctaGeometrySource.ts
@@ -1,12 +1,14 @@
+import { publicAssetUrl } from './publicAssetUrl';
 import { normalizeZipGeometry, type RawZipGeometry, type ZipGeometrySource } from './ZipGeometrySource';
 
-export const DEFAULT_STATIC_ZCTA_URL = '/data/cleveland-akron-zcta-2020.geojson';
+export const DEFAULT_STATIC_ZCTA_PATH = 'data/cleveland-akron-zcta-2020.geojson';
 
 export class StaticZctaGeometrySource implements ZipGeometrySource {
-  constructor(private readonly url = DEFAULT_STATIC_ZCTA_URL) {}
+  constructor(private readonly path = DEFAULT_STATIC_ZCTA_PATH) {}
 
   async load(zips: readonly string[]): Promise<RawZipGeometry> {
-    const response = await fetch(this.url);
+    const url = publicAssetUrl(this.path);
+    const response = await fetch(url);
     if (!response.ok) {
       throw new Error(
         `Unable to load checked-in Census-derived ZCTA geometry: ${response.status} ${response.statusText}`,
